dd2612206b5f90756be3183cc97065428e88acc7
iceburg333 <ice_burg333@yahoo.com>
2018-08-10 19:46:46 -0400

    creatures can have items

--- src/core/gameState/Creature.kt
+++ src/core/gameState/Creature.kt
@@ -2,14 +2,19 @@ package core.gameState
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import system.BodyManager
+import system.ItemManager
 
 class Creature(override val name: String, override val description: String, val body: Body = Body(), var location: Location = GameState.world, val parent: Target? = null, override val properties: Properties = Properties()) : Target {
 
-    @JsonCreator
-    constructor(name: String, description: String, body: String, properties: Properties = Properties()): this(name, description, BodyManager.getBody(body), properties = properties)
-
     val soul = Soul(this)
     val inventory = Inventory()
+
+    @JsonCreator
+    constructor(name: String, description: String, body: String, properties: Properties = Properties(), items: List<String>): this(name, description, BodyManager.getBody(body), properties = properties) {
+        items.forEach { inventory.add(ItemManager.getItem(it)) }
+    }
+
+
 
     init {
         properties.tags.add("Creature")
