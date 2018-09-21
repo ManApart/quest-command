77c298509d2f0382dc64b164b023c9aea7fbbe63
iceburg333 <ice_burg333@yahoo.com>
2018-09-21 10:24:50 -0400

    cleanup of duplicate code

--- src/main/java/core/utility/ReflectionTools.kt
+++ src/main/java/core/utility/ReflectionTools.kt
@@ -4,9 +4,9 @@ import core.commands.Command
 import core.commands.CommandParser.commands
 import core.events.Event
 import core.events.EventListener
-import core.history.ChatHistory.history
 import org.reflections.Reflections
 import java.io.File
+import kotlin.reflect.KClass
 import kotlin.reflect.full.memberProperties
 
 
@@ -15,76 +15,54 @@ object ReflectionTools {
     private const val commandsFile = "/data/generated/commands.txt"
     private const val eventsFile = "/data/generated/events.txt"
     private const val eventListenersFile = "/data/generated/eventListeners.txt"
+    private val reflections = Reflections()
 
-    fun saveAllCommands() : List<Class<out Command>> {
-        val reflections = Reflections()
-        val allClasses = reflections.getSubTypesOf(Command::class.java)
+    fun saveAllCommands() {
+        saveClassNamesToFile(Command::class, commandsFile)
+    }
 
-        File(srcPrefix + commandsFile).printWriter().use { out ->
+    fun saveAllEvents() {
+        saveClassNamesToFile(Event::class, eventsFile)
+    }
+
+    fun saveAllEventListeners() {
+        saveClassNamesToFile(EventListener::class, eventListenersFile)
+    }
+
+    private fun saveClassNamesToFile(clazz: KClass<*>, file: String) {
+        val allClasses = reflections.getSubTypesOf(clazz::class.java)
+
+        File(srcPrefix + file).printWriter().use { out ->
             allClasses.forEach {
                 out.println(it.name)
             }
         }
-
-        return allClasses.toList()
     }
 
     fun getAllCommands() : List<Class<out Command>> {
-        val commands = mutableListOf<Class<out Command>>()
-        val content = this::class.java.getResource(commandsFile).readText()
-        content.trim().split("\r\n").forEach {
-            val kClass: Class<out Command> = Class.forName(it) as Class<out Command>
-            commands.add(kClass)
-        }
-        return commands.toList()
+        return getClassesFromFile(commandsFile)
     }
 
-    fun saveAllEvents() : List<Class<out Event>> {
-        val reflections = Reflections()
-        val allClasses = reflections.getSubTypesOf(Event::class.java)
-        File(srcPrefix + eventsFile).printWriter().use { out ->
-            allClasses.forEach {
-                out.println(it.name)
-            }
-        }
-        return allClasses.toList()
+    fun getAllEvents() : List<Class<out Event>> {
+        return getClassesFromFile(eventsFile)
     }
 
     fun getEvent(className: String): Class<out Event> {
         return getAllEvents().first { className == it.name.substring(it.name.lastIndexOf(".")+1) }
     }
 
-    fun getAllEvents() : List<Class<out Event>> {
-        val events = mutableListOf<Class<out Event>>()
-        val content = this::class.java.getResource(eventsFile).readText()
-        content.trim().split("\r\n").forEach {
-            val kClass: Class<out Event> = Class.forName(it) as Class<out Event>
-            events.add(kClass)
-        }
-        return events.toList()
-    }
-
-    fun saveAllEventListeners() : List<Class<out EventListener<*>>> {
-        val reflections = Reflections()
-        val allClasses = reflections.getSubTypesOf(EventListener::class.java)
-
-        File(srcPrefix + eventListenersFile).printWriter().use { out ->
-            allClasses.forEach {
-                out.println(it.name)
-            }
-        }
-
-        return allClasses.toList()
-    }
-
     fun getAllEventListeners() : List<Class<out EventListener<*>>> {
-        val listeners = mutableListOf<Class<out EventListener<*>>>()
-        val content = this::class.java.getResource(eventListenersFile).readText()
+        return getClassesFromFile(eventListenersFile)
+    }
+
+    private fun <E>  getClassesFromFile(file: String) : List<Class<E>> {
+        val classes = mutableListOf<Class<E>>()
+        val content = this::class.java.getResource(file).readText()
         content.trim().split("\r\n").forEach {
-            val kClass: Class<out EventListener<*>> = Class.forName(it) as Class<out EventListener<*>>
-            listeners.add(kClass)
+            val kClass = Class.forName(it) as Class<E>
+            classes.add(kClass)
         }
-        return listeners.toList()
+        return classes.toList()
     }
 
     fun <R: Any> getProperty(instance: Any, propertyName: String) : R {
