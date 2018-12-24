f1dd7ab71b3c43291ed5ec19562c66a7c24aeda5
iceburg333 <ice_burg333@yahoo.com>
2018-12-24 13:35:21 -0500

    fix typos

--- src/integration-test/kotlin/commandCombos/CommandComboTest.kt
+++ src/integration-test/kotlin/commandCombos/CommandComboTest.kt
@@ -58,9 +58,9 @@ class CommandComboTest {
     @Test
     fun climbTree() {
         //TODO - somehow remove random element and test full attemptClimb
-        val input = "n && attemptClimb tree"
+        val input = "n && climb tree"
         CommandParser.parseCommand(input)
-        assertEquals(("You are 1ft up. Above you are path choices 1, 2. You are at the bottom of Apple Tree."), ChatHistory.getLastOutput())
+        assertEquals(("Above you are path choices 1, 2. You are at the bottom of Apple Tree."), ChatHistory.getLastOutput())
     }
 
     @Test
