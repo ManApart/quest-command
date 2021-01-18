8c839d635c87c9560896fc1bafe239409385b2a2
ManApart <raustinkucera@gmail.com>
2021-01-18 17:05:52 -0500

    uncomment reflection  tools

--- src/main/kotlin/core/reflection/ReflectionTools.kt
+++ src/main/kotlin/core/reflection/ReflectionTools.kt
@@ -21,103 +21,103 @@ object ReflectionTools {
     }
 
     fun generateFiles() {
-//        generateCollectionsFile(Command::class.java)
-//        generateCollectionsFile(SpellCommand::class.java)
-//        generateCollectionsFile(EventParser::class.java)
-//        generateCollectionsFile(EventListener::class.java)
-//        generateResourcesFile(BehaviorResource::class.java, Behavior::class.java)
+        generateCollectionsFile(Command::class.java)
+        generateCollectionsFile(SpellCommand::class.java)
+        generateCollectionsFile(EventParser::class.java)
+        generateCollectionsFile(EventListener::class.java)
+        generateResourcesFile(BehaviorResource::class.java, Behavior::class.java)
     }
 
-//    private fun generateCollectionsFile(clazz: Class<*>) {
-//        val allClasses = reflections.getSubTypesOf(clazz).filter { !Modifier.isAbstract(it.modifiers) }.sortedBy { it.name }
-//        println("Saving ${allClasses.size} classes for ${clazz.name}")
-//        val isTyped = clazz.typeParameters.isNotEmpty()
-//        val typeSuffix = if(isTyped){
-//            "<*>"
-//        } else {
-//            ""
-//        }
-//
-//        val classes = allClasses.joinToString(", ") { "${it.name}()".replace("$", ".") }
-//
-//        writeInterfaceFile(clazz, typeSuffix)
-//        writeGeneratedFile(clazz, typeSuffix, classes)
-//        writeMockedFile(clazz, typeSuffix)
-//    }
-//
-//    private fun generateResourcesFile(clazz: Class<*>, resourceClass: Class<*>) {
-//        val allClasses= reflections.getSubTypesOf(clazz).filter { !Modifier.isAbstract(it.modifiers) }.sortedBy { it.name }
-//        println("Saving ${allClasses.size} classes for ${clazz.name}")
-//        val isTyped = resourceClass.typeParameters.isNotEmpty()
-//        val typeSuffix = if(isTyped){
-//            "<*>"
-//        } else {
-//            ""
-//        }
-//        val classes = allClasses.joinToString(", ") { "${it.name}()".replace("$", ".") }
-//
-//        writeInterfaceFile(resourceClass, typeSuffix)
-//        writeGeneratedResourceFile(resourceClass, typeSuffix, classes)
-//        writeMockedFile(resourceClass, typeSuffix)
-//    }
-//
-//    private fun writeInterfaceFile(clazz: Class<*>, typeSuffix: String) {
-//        val packageName = clazz.packageName.replace(".", "/")
-//        File("./src/main/kotlin/$packageName/${clazz.simpleName}sCollection.kt").printWriter().use {
-//            it.print(
-//                """
-//                package ${clazz.packageName}
-//
-//                interface ${clazz.simpleName}sCollection {
-//                    val values: List<${clazz.simpleName}$typeSuffix>
-//                }
-//            """.trimIndent()
-//            )
-//        }
-//    }
-//
-//    private fun writeGeneratedFile(clazz: Class<*>, typeSuffix: String, classes: String) {
-//        val packageName = clazz.packageName.replace(".", "/")
-//        File("./src/main/kotlin/$packageName/${clazz.simpleName}sGenerated.kt").printWriter().use {
-//            it.print(
-//                    """
-//                package ${clazz.packageName}
-//
-//                class ${clazz.simpleName}sGenerated : ${clazz.simpleName}sCollection {
-//                    override val values: List<${clazz.name}$typeSuffix> = listOf($classes)
-//                }
-//            """.trimIndent()
-//            )
-//        }
-//    }
-//
-//    private fun writeGeneratedResourceFile(clazz: Class<*>, typeSuffix: String, classes: String) {
-//        val packageName = clazz.packageName.replace(".", "/")
-//
-//        File("./src/main/kotlin/$packageName/${clazz.simpleName}sGenerated.kt").printWriter().use {
-//            it.print(
-//                    """
-//                package ${clazz.packageName}
-//
-//                class ${clazz.simpleName}sGenerated : ${clazz.simpleName}sCollection {
-//                    override val values: List<${clazz.simpleName}$typeSuffix> = listOf($classes).flatMap { it.values }
-//                }
-//            """.trimIndent()
-//            )
-//        }
-//    }
-//
-//    private fun writeMockedFile(clazz: Class<*>, typeSuffix: String) {
-//        val packageName = clazz.packageName.replace(".", "/")
-//        File("./src/test/kotlin/$packageName/${clazz.simpleName}sMock.kt").printWriter().use {
-//            it.print(
-//                    """
-//                package ${clazz.packageName}
-//
-//                class ${clazz.simpleName}sMock(override val values: List<${clazz.simpleName}$typeSuffix> = listOf()) : ${clazz.simpleName}sCollection
-//            """.trimIndent()
-//            )
-//        }
-//    }
+    private fun generateCollectionsFile(clazz: Class<*>) {
+        val allClasses = reflections.getSubTypesOf(clazz).filter { !Modifier.isAbstract(it.modifiers) }.sortedBy { it.name }
+        println("Saving ${allClasses.size} classes for ${clazz.name}")
+        val isTyped = clazz.typeParameters.isNotEmpty()
+        val typeSuffix = if(isTyped){
+            "<*>"
+        } else {
+            ""
+        }
+
+        val classes = allClasses.joinToString(", ") { "${it.name}()".replace("$", ".") }
+
+        writeInterfaceFile(clazz, typeSuffix)
+        writeGeneratedFile(clazz, typeSuffix, classes)
+        writeMockedFile(clazz, typeSuffix)
+    }
+
+    private fun generateResourcesFile(clazz: Class<*>, resourceClass: Class<*>) {
+        val allClasses= reflections.getSubTypesOf(clazz).filter { !Modifier.isAbstract(it.modifiers) }.sortedBy { it.name }
+        println("Saving ${allClasses.size} classes for ${clazz.name}")
+        val isTyped = resourceClass.typeParameters.isNotEmpty()
+        val typeSuffix = if(isTyped){
+            "<*>"
+        } else {
+            ""
+        }
+        val classes = allClasses.joinToString(", ") { "${it.name}()".replace("$", ".") }
+
+        writeInterfaceFile(resourceClass, typeSuffix)
+        writeGeneratedResourceFile(resourceClass, typeSuffix, classes)
+        writeMockedFile(resourceClass, typeSuffix)
+    }
+
+    private fun writeInterfaceFile(clazz: Class<*>, typeSuffix: String) {
+        val packageName = clazz.packageName.replace(".", "/")
+        File("./src/main/kotlin/$packageName/${clazz.simpleName}sCollection.kt").printWriter().use {
+            it.print(
+                """
+                package ${clazz.packageName}
+
+                interface ${clazz.simpleName}sCollection {
+                    val values: List<${clazz.simpleName}$typeSuffix>
+                }
+            """.trimIndent()
+            )
+        }
+    }
+
+    private fun writeGeneratedFile(clazz: Class<*>, typeSuffix: String, classes: String) {
+        val packageName = clazz.packageName.replace(".", "/")
+        File("./src/main/kotlin/$packageName/${clazz.simpleName}sGenerated.kt").printWriter().use {
+            it.print(
+                    """
+                package ${clazz.packageName}
+
+                class ${clazz.simpleName}sGenerated : ${clazz.simpleName}sCollection {
+                    override val values: List<${clazz.name}$typeSuffix> = listOf($classes)
+                }
+            """.trimIndent()
+            )
+        }
+    }
+
+    private fun writeGeneratedResourceFile(clazz: Class<*>, typeSuffix: String, classes: String) {
+        val packageName = clazz.packageName.replace(".", "/")
+
+        File("./src/main/kotlin/$packageName/${clazz.simpleName}sGenerated.kt").printWriter().use {
+            it.print(
+                    """
+                package ${clazz.packageName}
+
+                class ${clazz.simpleName}sGenerated : ${clazz.simpleName}sCollection {
+                    override val values: List<${clazz.simpleName}$typeSuffix> = listOf($classes).flatMap { it.values }
+                }
+            """.trimIndent()
+            )
+        }
+    }
+
+    private fun writeMockedFile(clazz: Class<*>, typeSuffix: String) {
+        val packageName = clazz.packageName.replace(".", "/")
+        File("./src/test/kotlin/$packageName/${clazz.simpleName}sMock.kt").printWriter().use {
+            it.print(
+                    """
+                package ${clazz.packageName}
+
+                class ${clazz.simpleName}sMock(override val values: List<${clazz.simpleName}$typeSuffix> = listOf()) : ${clazz.simpleName}sCollection
+            """.trimIndent()
+            )
+        }
+    }
 
 }
