58e74978fbc6ee7267621ea374e5fdfc559829a8
ManApart <r@austinkucera.com>
2021-08-24 07:45:12 -0400

    extract example ai to wiki

--- src/main/kotlin/resources/ai/CommonAI.kt
+++ src/main/kotlin/resources/ai/CommonAI.kt
@@ -5,11 +5,6 @@ import core.ai.dsl.aiBuilder
 
 class CommonAI : AIResource {
     override val values = aiBuilder {
-        ai("name") {
-            inherits = listOf("parent")
-            additional = listOf("action")
-            exclude = listOf("action")
-        }
         ai("Cowardly Predator") {
             additional = listOf("Nothing", "Rat Attack")
         }
