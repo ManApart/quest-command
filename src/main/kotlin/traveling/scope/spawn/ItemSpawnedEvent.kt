acd193a7059959dc93b79f245559a690ebdaba55
ManApart <r@austinkucera.com>
2021-10-01 21:51:53 -0400

    remove comment

--- src/main/kotlin/traveling/scope/spawn/ItemSpawnedEvent.kt
+++ src/main/kotlin/traveling/scope/spawn/ItemSpawnedEvent.kt
@@ -3,5 +3,5 @@ package traveling.scope.spawn
 import core.events.Event
 import core.target.Target
 import traveling.location.network.LocationNode
-//Rename to spawn item event
+
 class ItemSpawnedEvent(val item: Target, val target: Target?, val targetLocation: LocationNode) : Event
