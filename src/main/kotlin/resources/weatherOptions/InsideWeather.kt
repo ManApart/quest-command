7fc593fa089f46952ea0e2da45a9719754a22fcb
ManApart <r@austinkucera.com>
2021-10-08 09:31:09 -0400

    more weather sounds

--- src/main/kotlin/resources/weatherOptions/InsideWeather.kt
+++ src/main/kotlin/resources/weatherOptions/InsideWeather.kt
@@ -8,8 +8,17 @@ class InsideWeather : WeatherResource {
     override val values = weathers {
         weather("Stifled Air", "The air is stale and stifled.")
         weather("Calm", "The air is warm and still.")
-        weather("Distant Rain", "You can hear the rain falling outside.")
-        weather("Distant Storm", "You can hear the cracks of thunder outside.")
+        
+        weather("Distant Rain" ){
+            description("You can hear the rain falling outside.")
+            sound(3, "the percussion of rain outside mixed with sloshing, streaming and splashing")
+        }
+
+        weather("Distant Storm" ){
+            description("You can hear the cracks of thunder outside.")
+            sound(4, "the distant and yet heavy slap of rain deepened by rolling rumbles and loud claps")
+        }
+
         weather("Claustrophobic", "The air is tightly packed and feels dense.")
         weather("Dusty", "You can see motes of dust crowding the air.")
         weather("Earthy", "You smell earthy tones and taste grit against your lips.")
