ff493aed6175bf3c46c1db73ef8ebac54402bb34
ManApart <r@austinkucera.com>
2021-10-07 20:12:00 -0400

    convert inside weathers

--- src/main/kotlin/resources/weatherOptions/InsideWeather.kt
+++ src/main/kotlin/resources/weatherOptions/InsideWeather.kt
@@ -1,18 +1,19 @@
 package resources.weatherOptions
 
-import traveling.location.weather.Weather
 import traveling.location.weather.WeatherResource
+import traveling.location.weather.build
+import traveling.location.weather.weathers
 
 class InsideWeather : WeatherResource {
-    override val values = listOf(
-        Weather("Stifled Air", "The air is stale and stifled."),
-        Weather("Calm", "The air is warm and still."),
-        Weather("Distant Rain", "You can hear the rain falling outside."),
-        Weather("Distant Storm", "You can hear the cracks of thunder outside."),
-        Weather("Claustrophobic", "The air is tightly packed and feels dense."),
-        Weather("Dusty", "You can see motes of dust crowding the air."),
-        Weather("Earthy", "You smell earthy tones and taste grit against your lips."),
-        Weather("Rocky", "You smell earthy tones and taste grit against your lips."),
-        Weather("Overgrown", "Spores, pollen and plant life surge into your nostrils."),
-    )
+    override val values = weathers {
+        weather("Stifled Air", "The air is stale and stifled.")
+        weather("Calm", "The air is warm and still.")
+        weather("Distant Rain", "You can hear the rain falling outside.")
+        weather("Distant Storm", "You can hear the cracks of thunder outside.")
+        weather("Claustrophobic", "The air is tightly packed and feels dense.")
+        weather("Dusty", "You can see motes of dust crowding the air.")
+        weather("Earthy", "You smell earthy tones and taste grit against your lips.")
+        weather("Rocky", "You smell earthy tones and taste grit against your lips.")
+        weather("Overgrown", "Spores, pollen and plant life surge into your nostrils.")
+    }.build()
 }
