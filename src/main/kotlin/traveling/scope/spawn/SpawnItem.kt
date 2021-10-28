fc4d86b33891f7a14bd31f98d0ed8bf23b2972c4
ManApart <r@austinkucera.com>
2021-10-28 18:56:21 -0400

    change display timing

--- src/main/kotlin/traveling/scope/spawn/SpawnItem.kt
+++ src/main/kotlin/traveling/scope/spawn/SpawnItem.kt
@@ -10,9 +10,9 @@ class SpawnItem : EventListener<ItemSpawnedEvent>() {
     override fun execute(event: ItemSpawnedEvent) {
         if (event.thing == null) {
             val name = (event.item.properties.getCount() > 1).then("${event.item.properties.getCount()}x ${event.item.name}s", event.item.name)
-            event.item.display("$name appeared.")
             event.item.location = event.thingLocation
             event.item.location.getLocation().addThing(event.item)
+            event.item.display("$name appeared.")
         } else {
             event.thing.inventory.add(event.item)
             EventManager.postEvent(ItemPickedUpEvent(event.thing, event.item, true))
