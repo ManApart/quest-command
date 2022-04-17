de4a14de3ca603b9dfbfbb17c83f11e886ab306a
ManApart <r@austinkucera.com>
2022-04-17 10:16:53 -0400

    wip web call

--- src/main/kotlin/system/connection/WebClient.kt
+++ src/main/kotlin/system/connection/WebClient.kt
@@ -1,16 +1,26 @@
 package system.connection
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
 //TODO - how do we poll for updates?
 object WebClient {
     var host = "localhost"
     var port = "8080"
+    private val client by lazy { HttpClient.newBuilder().build() }
 
 
     fun getServerInfo(): String {
         return "$host:$port"
     }
 
-    fun isValidServer(host: String, port: String): Boolean{
+    fun isValidServer(host: String, port: String): Boolean {
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create("$host:$port"))
+            .build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
         return false
     }
 
