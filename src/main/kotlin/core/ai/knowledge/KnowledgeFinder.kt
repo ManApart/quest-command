d40d2f968c3b972db450f11ec08c588b6168f4fd
ManApart <r@austinkucera.com>
2022-05-13 08:07:50 -0400

    more fact processing

--- src/main/kotlin/core/ai/knowledge/KnowledgeFinder.kt
+++ src/main/kotlin/core/ai/knowledge/KnowledgeFinder.kt
@@ -1,4 +1,21 @@
 package core.ai.knowledge
 
-class KnowledgeFinder(val findFact: (Mind)-> Fact) {
+class KnowledgeFinder(
+    private val kind: String,
+    private val relevantSource: (Subject) -> Boolean = { true },
+    private val relevantRelatesTo: (Subject) -> Boolean = { true },
+    val findFact: ((mind: Mind, source: Subject, kind: String) -> Fact)? = null,
+    val findRelationship: ((mind: Mind, source: Subject, kind: String, relatesTo: Subject) -> Relationship)? = null
+) {
+    init {
+        require(findFact != null || findRelationship != null) { "Find fact or find relationship must not be null!" }
+    }
+
+    fun matches(source: Subject, kind: String, relatesTo: Subject): Boolean {
+        return this.kind.equals(kind, ignoreCase = true) && relevantSource(source) && relevantRelatesTo(relatesTo)
+    }
+
+    fun matches(source: Subject, kind: String): Boolean {
+        return this.kind.equals(kind, ignoreCase = true) && relevantSource(source)
+    }
 }
--- src/main/kotlin/core/ai/knowledge/Mind.kt
+++ src/main/kotlin/core/ai/knowledge/Mind.kt
@@ -2,25 +2,24 @@ package core.ai.knowledge
 
 import core.thing.Thing
 
+//TODO - move to some mind manager or something
+val finders: List<KnowledgeFinder> = listOf()
+
 class Mind {
     lateinit var creature: Thing
     private val personalFacts = mutableMapOf<String, List<Fact>>()
     private val personalRelationships = mutableMapOf<String, List<Relationship>>()
 
-    fun knows(kind: String, relatesTo: Subject): Relationship {
-        return knows(Subject(creature), relatesTo, kind)
+    fun knows(kind: String, relatesTo: Subject) = knows(Subject(creature), kind, relatesTo)
+    fun knows(source: Subject, kind: String, relatesTo: Subject): Relationship {
+        val foundFacts = finders.filter { it.matches(source, kind, relatesTo) }.mapNotNull { it.findRelationship?.invoke(this, source, kind, relatesTo) }
+        return Relationship(source, kind, relatesTo, foundFacts.sumOf { it.confidence }, foundFacts.sumOf { it.amount })
     }
 
-    fun knows(source: Subject, relatesTo: Subject, kind: String): Relationship {
-        return Relationship(Subject(""), "", Subject(""), 0, 0)
-    }
-
-    fun knowsFact(kind: String): Fact {
-        return knowsFact(Subject(creature), kind)
-    }
-
+    fun knowsFact(kind: String) = knowsFact(Subject(creature), kind)
     fun knowsFact(source: Subject, kind: String): Fact {
-        return Fact(Subject(""), "", 0, 0)
+        val foundFacts = finders.filter { it.matches(source, kind) }.mapNotNull { it.findFact?.invoke(this, source, kind) }
+        return Fact(source, kind, foundFacts.sumOf { it.confidence }, foundFacts.sumOf { it.amount })
     }
 
 }
