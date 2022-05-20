b5108921838f1deaabeb6f533f5d69c5add57b3a
ManApart <r@austinkucera.com>
2022-05-20 10:43:25 -0400

    save outputs to clients in mp

--- src/main/kotlin/system/persistance/saving/Save.kt
+++ src/main/kotlin/system/persistance/saving/Save.kt
@@ -2,12 +2,15 @@ package system.persistance.saving
 
 import core.GameState
 import core.events.EventListener
+import core.history.displayGlobal
 import system.persistance.save
 
 class Save : EventListener<SaveEvent>() {
     override fun execute(event: SaveEvent) {
         save(GameState.gameName)
 
-        println("Saved ${event.source.thing.name} in ${GameState.gameName}.")
+        if (!event.silent) {
+            displayGlobal("Saved ${event.source.thing.name} in ${GameState.gameName}.")
+        }
     }
 }
--- src/main/kotlin/system/persistance/saving/SaveEvent.kt
+++ src/main/kotlin/system/persistance/saving/SaveEvent.kt
@@ -3,4 +3,4 @@ package system.persistance.saving
 import core.Player
 import core.events.Event
 
-class SaveEvent(val source: Player) : Event
+class SaveEvent(val source: Player, val silent: Boolean = false) : Event
