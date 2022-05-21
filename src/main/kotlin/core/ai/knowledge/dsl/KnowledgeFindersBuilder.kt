a30aa7c81decf2947d02e2d02a5c7f062f6661fc
ManApart <r@austinkucera.com>
2022-05-21 11:03:55 -0400

    wip clean up kind children

--- src/main/kotlin/core/ai/knowledge/dsl/KnowledgeFindersBuilder.kt
+++ src/main/kotlin/core/ai/knowledge/dsl/KnowledgeFindersBuilder.kt
@@ -2,45 +2,29 @@ package core.ai.knowledge.dsl
 
 import core.ai.knowledge.KnowledgeFinder
 
-class KnowledgeFindersBuilder {
+class KnowledgeFindersBuilder(private val kind: String? = null) {
     private val facts = mutableListOf<FactFinderBuilder>()
     private val relationships = mutableListOf<RelationshipFinderBuilder>()
-    private val kindChildren = mutableListOf<KnowledgeFindersKindBuilder>()
+    private val kindChildren = mutableListOf<KnowledgeFindersBuilder>()
 
     fun fact(initializer: FactFinderBuilder.() -> Unit) {
-        facts.add(FactFinderBuilder().apply(initializer))
+        val builder = if (kind != null) FactFinderBuilder().apply { kind(kind) } else FactFinderBuilder()
+        facts.add(builder.apply(initializer))
     }
+
 //
 //    fun relationship(kind: String, initializer: RelationshipFinderBuilder.() -> Unit) {
 //        relationships.add(RelationshipFinderBuilder(kind).apply(initializer))
 //    }
 
-    fun kind(kind: String, initializer: KnowledgeFindersKindBuilder.() -> Unit) {
-        kindChildren.add(KnowledgeFindersKindBuilder(kind).apply(initializer))
+    fun kind(kind: String, initializer: KnowledgeFindersBuilder.() -> Unit) {
+        kindChildren.add(KnowledgeFindersBuilder(kind).apply(initializer))
     }
 
     fun build(): List<KnowledgeFinder> {
         return facts.map { KnowledgeFinder(it.build()) } +
                 relationships.map { KnowledgeFinder(it.build()) } +
                 kindChildren.flatMap { it.build() }
-    }
-}
-
-//Used only to allow grouping by kind in the main builder
-class KnowledgeFindersKindBuilder(private val kind: String) {
-    private val facts = mutableListOf<FactFinderBuilder>()
-    private val relationships = mutableListOf<RelationshipFinderBuilder>()
-
-    fun fact(initializer: FactFinderBuilder.() -> Unit) {
-        facts.add(FactFinderBuilder().apply { kind(kind) }.apply(initializer))
-    }
-
-    fun relationship(initializer: RelationshipFinderBuilder.() -> Unit) {
-        relationships.add(RelationshipFinderBuilder(kind).apply(initializer))
-    }
-
-    fun build(): List<KnowledgeFinder> {
-        return facts.map { KnowledgeFinder(it.build()) } + relationships.map { KnowledgeFinder(it.build()) }
     }
 }
 
