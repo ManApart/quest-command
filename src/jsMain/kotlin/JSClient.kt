e9016f790d3013ece740058899938fea9ff37bbc
ManApart <r@austinkucera.com>
2022-06-11 10:22:45 -0400

    clear screen

--- src/jsMain/kotlin/JSClient.kt
+++ src/jsMain/kotlin/JSClient.kt
@@ -4,17 +4,14 @@ import core.commands.CommandParsers
 import core.events.EventManager
 import core.history.GameLogger
 import core.history.InputOutput
-import core.history.displayToMe
 import kotlinx.browser.document
 import kotlinx.browser.window
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.HTMLInputElement
 
 private lateinit var outputDiv: Element
+private var historyStart = 0
 
 fun main() {
     window.onload = { document.startClient() }
@@ -32,33 +29,44 @@ fun Document.startClient() {
     updateOutput()
     window.onkeypress = { keyboardEvent ->
         if (keyboardEvent.key == "Enter") {
-            CommandParsers.parseCommand(GameState.player, prompt.value)
+            val input = prompt.value
             prompt.value = ""
-            updateOutput()
+            if (input.lowercase() == "clear") {
+                clearScreen()
+            } else {
+                CommandParsers.parseCommand(GameState.player, input)
+                updateOutput()
+            }
         }
     }
     scrollToBottom()
 }
 
-fun addHistoryMessageAfterCallback(message: String){
+fun addHistoryMessageAfterCallback(message: String) {
     /*
         The history was closed while we were doing the callback, so we need to add to the previous history
         Then refresh the page with the output
      */
     val histories = GameLogger.getMainHistory().history
-    histories[histories.size-1].outPut.add(message)
+    histories[histories.size - 1].outPut.add(message)
     updateOutput()
 }
 
 fun updateOutput() {
-    outputDiv.innerHTML = GameLogger.getMainHistory().history.joinToString("<br>") { it.toHtml() }
+    val history = GameLogger.getMainHistory().history
+    outputDiv.innerHTML = history.subList(historyStart, history.size).joinToString("<br>") { it.toHtml() }
     scrollToBottom()
 }
 
 private fun InputOutput.toHtml(): String {
-    return "<br/>> $input<br/>${outPut.joinToString("<br/>") {it.replace("\n", "<br/>")}}"
+    return "<br/>> $input<br/>${outPut.joinToString("<br/>") { it.replace("\n", "<br/>") }}"
 }
 
 private fun scrollToBottom() {
     window.scrollTo(0.0, document.body?.scrollHeight?.toDouble() ?: 100.0)
+}
+
+private fun clearScreen() {
+    historyStart = GameLogger.getMainHistory().history.size
+    updateOutput()
 }
