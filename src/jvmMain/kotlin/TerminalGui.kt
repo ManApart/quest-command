1ab032741cbad8694730d93dfa182e536ed7914b
ManApart <r@austinkucera.com>
2022-06-15 18:04:44 -0400

    parse and display commands

--- src/jvmMain/kotlin/TerminalGui.kt
+++ src/jvmMain/kotlin/TerminalGui.kt
@@ -1,6 +1,11 @@
+import core.GameState
+import core.commands.CommandParsers
+import core.history.GameLogger
+import core.utility.minOverlap
 import java.awt.*
 import java.awt.event.*
 import javax.swing.JFrame
+import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JScrollPane
 import javax.swing.JTextArea
@@ -14,11 +19,19 @@ object CSS {
     val highlight = Color.gray
     val font = Font("Monaco", Font.PLAIN, 20)
 }
+
 fun runInGui() {
+    CommandParsers.parseInitialCommand(GameState.player)
     TerminalGui()
 }
 
-class TerminalGui : JFrame(){
+class TerminalGui : JFrame() {
+    private val output: JTextArea
+    private val suggestionsArea: JTextArea
+    private val prompt: JTextField
+    private var historyStart = 0
+    private var suggestions = listOf<String>()
+
     init {
         title = "Quest Command"
         iconImage = Toolkit.getDefaultToolkit().getImage(this::class.java.getResource("favicon.png"))
@@ -28,7 +41,7 @@ class TerminalGui : JFrame(){
         layout = BorderLayout()
 
         val sampleText = (1..100).joinToString { "This is some text" }
-        val output = JTextArea(sampleText).apply {
+        output = JTextArea(sampleText).apply {
             isEditable = false
             lineWrap = true
             background = CSS.background
@@ -45,33 +58,12 @@ class TerminalGui : JFrame(){
         }
         contentPane.add(outputScroll, BorderLayout.CENTER)
 
-        val bottomPart = JPanel().apply { layout = BorderLayout() }
-
-        val prompt = JTextField("Inital").apply {
+        val bottomPart = JPanel().apply {
             background = CSS.background
-            foreground = CSS.text
-            border = EmptyBorder(10, 10, 10, 10)
-            font = CSS.font
-
-            addFocusListener(object : FocusListener{
-                override fun focusGained(e: FocusEvent?) {
-                    println("Focus gained")
-                }
-
-                override fun focusLost(e: FocusEvent?) {
-                    this@apply.grabFocus()
-                }
-
-            })
-            addActionListener {
-                println("action performend $it")
-                output.append(text)
-                text = ""
-            }
+            layout = BorderLayout()
         }
-        bottomPart.add(prompt, BorderLayout.CENTER)
 
-        val suggestions = JTextArea("option option option").apply {
+        suggestionsArea = JTextArea().apply {
             isEditable = false
             lineWrap = true
             background = CSS.background
@@ -79,11 +71,86 @@ class TerminalGui : JFrame(){
             border = EmptyBorder(10, 10, 10, 10)
             font = CSS.font
         }
-        bottomPart.add(suggestions, BorderLayout.SOUTH)
+        bottomPart.add(suggestionsArea, BorderLayout.SOUTH)
+
+        prompt = JTextField().apply {
+            background = CSS.background
+            foreground = CSS.text
+            border = EmptyBorder(10, 10, 10, 10)
+            font = CSS.font
+            caretColor = Color.white
+
+            addKeyListener(object : KeyListener {
+                override fun keyTyped(e: KeyEvent) {}
+                override fun keyPressed(e: KeyEvent) {}
+                override fun keyReleased(e: KeyEvent) = tabHint()
+            })
+
+            addFocusListener(object : FocusListener {
+                override fun focusGained(e: FocusEvent?) {}
+                override fun focusLost(e: FocusEvent?) {
+                    tabComplete()
+                    this@apply.grabFocus()
+                }
+            })
+            addActionListener {
+                CommandParsers.parseCommand(GameState.player, text)
+                text = ""
+                updateOutput()
+                suggestionsArea.text = ""
+            }
+        }
+        bottomPart.add(prompt, BorderLayout.CENTER)
+
+        bottomPart.add(JLabel(">").apply {
+            foreground = CSS.text
+            border = EmptyBorder(10, 10, 10, 10)
+            font = CSS.font
+        }, BorderLayout.WEST)
 
         contentPane.add(bottomPart, BorderLayout.SOUTH)
 
-
+        updateOutput()
+        prompt.grabFocus()
         pack()
+    }
+
+    private fun updateOutput() {
+        val history = GameLogger.getMainHistory().history
+        output.text = history.subList(historyStart, history.size).joinToString("\n") {
+            "\n> ${it.input}\n${it.outPut.joinToString("\n")}"
+        }
+    }
+
+    private fun tabComplete() {
+        val input = prompt.text.split(" ").lastOrNull()
+        if (!input.isNullOrBlank()) {
+            val prePrompt = prompt.text.substring(0, prompt.text.indexOf(input))
+            val overlap = suggestions.minOverlap()
+            println("Suggestions: ${suggestions.joinToString()}")
+            when {
+                suggestions.size == 1 -> prompt.text = prePrompt + suggestions.first()
+                suggestions.size > 1 && overlap.length > input.length -> prompt.text = prePrompt + overlap
+            }
+            tabHint()
+        }
+    }
+
+    private fun tabHint() {
+        val input = prompt.text.lowercase()
+        val lastInput = input.split(" ").lastOrNull()
+        if (input.isNotBlank()) {
+            val allSuggestions = CommandParsers.suggestions(GameState.player, input)
+            suggestions = if (lastInput.isNullOrBlank()) {
+                allSuggestions
+            } else {
+                allSuggestions.filter {
+                    val option = it.lowercase()
+                    option.startsWith(lastInput)
+                }
+            }
+
+            suggestionsArea.text = suggestions.joinToString(" ")
+        }
     }
 }
