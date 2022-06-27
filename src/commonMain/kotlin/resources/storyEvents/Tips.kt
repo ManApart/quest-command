ee27c548947fc8d489e5538f48a987a1d85a14d5
ManApart <r@austinkucera.com>
2022-06-27 07:36:41 -0400

    rest tip

--- src/commonMain/kotlin/resources/storyEvents/Tips.kt
+++ src/commonMain/kotlin/resources/storyEvents/Tips.kt
@@ -6,6 +6,8 @@ import quests.ConditionalEvents
 import quests.StoryEvent
 import quests.StoryEventResource
 import status.LevelUpEvent
+import status.stat.STAMINA
+import status.statChanged.StatMinnedEvent
 import system.message.MessageEvent
 
 class Tips : StoryEventResource {
@@ -14,6 +16,12 @@ class Tips : StoryEventResource {
             ConditionalEvents(LevelUpEvent::class,
                 { event, _ -> event.source.isPlayer() },
                 { event, _ -> listOf(MessageEvent(GameState.getPlayer(event.source)!!, "When I level up I should rest to restore my stats to their new levels.")) }
+            ), availableBefore = 1000, availableAfter = 0
+        ),
+        StoryEvent("Tips", 2, "I can rest to restore my stamina. I can rest by using the rest command and supplying how many hours to rest.",
+            ConditionalEvents(StatMinnedEvent::class,
+                { event, _ -> event.thing.isPlayer() && event.stat == STAMINA },
+                { event, _ -> listOf(MessageEvent(GameState.getPlayer(event.thing)!!, "My stamina is empty. I should rest until it's recovered. I can rest by using the rest command and supplying how many hours to rest.")) }
             ), availableBefore = 1000, availableAfter = 0
         ),
     )
