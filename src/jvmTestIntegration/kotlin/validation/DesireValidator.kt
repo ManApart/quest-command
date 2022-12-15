e97d3ec0b037065ddca0450756cdd025eb87c4bf
ManApart <r@austinkucera.com>
2022-12-15 08:22:19 -0500

    agenda reference validation

--- src/jvmTestIntegration/kotlin/validation/DesireValidator.kt
+++ src/jvmTestIntegration/kotlin/validation/DesireValidator.kt
@@ -1,24 +1,35 @@
 package validation
 
 import core.ai.AIManager
-import core.utility.toNameSearchableList
 
 
 class DesireValidator {
 
     private val desires = AIManager.desires
-    private val agendas = AIManager.agendas.toNameSearchableList()
+    private val agendas = AIManager.agendas
 
     fun validate(): Int {
-        return noGoalsWithoutAgendas()
+        return noGoalsWithoutAgendas() + noAgendaOrphans()
     }
 
     private fun noGoalsWithoutAgendas(): Int {
         var warnings = 0
         desires.forEach { desireTree ->
             desireTree.getAllDesires().map { it.first }.forEach { desire ->
-                if (agendas.getOrNull(desire) == null) {
+                if (agendas[desire] == null) {
                     println("WARN: Could not find agenda '${desire}' for desire '${desire}'.")
+                    warnings++
+                }
+            }
+        }
+        return warnings
+    }
+    private fun noAgendaOrphans(): Int {
+        var warnings = 0
+        agendas.values.forEach { agenda ->
+            agenda.steps.mapNotNull { it.agendaName }.forEach { reference ->
+                if (agendas[reference] == null){
+                    println("WARN: Could not find agenda '${reference}' referenced by agenda '${agenda}'.")
                     warnings++
                 }
             }
