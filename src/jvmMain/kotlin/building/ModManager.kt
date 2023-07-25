a4843b2ae9e61d4caa218bdb2b80f05c5d1ad315
ManApart <r@austinkucera.com>
2023-07-25 08:20:00 -0400

    loading mod from jar

--- src/jvmMain/kotlin/building/ModManager.kt
+++ src/jvmMain/kotlin/building/ModManager.kt
@@ -1,5 +1,7 @@
 package building
 
+import core.thing.item.ItemResource
+import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.net.URL
 import java.net.URLClassLoader
@@ -17,20 +19,22 @@ Resolve load order conflicts
 actual object ModManager {
     actual fun applyMods() {
         val manifestFile = File("./mods/manifest.txt")
-        val manifest = if (manifestFile.exists()){
+        val manifest = if (manifestFile.exists()) {
             manifestFile.readLines()
         } else listOf()
         println("Found ${manifest.size} mods.")
 
-        manifest.mapNotNull { fileName ->
-            val jarFile = File("./mods/$fileName.jar")
-            if (jarFile.exists()) jarFile else null
-        }.forEach { loadJar(it) }
-
+        runBlocking {
+            manifest.mapNotNull { fileName ->
+                val jarFile = File("./mods/$fileName.jar")
+                if (jarFile.exists()) jarFile else null
+            }.forEach { loadJar(it) }
+        }
         if (manifest.isNotEmpty()) println("Loaded Mods")
     }
 
-    private fun loadJar(jarFile: File) {
+    @Suppress("UNCHECKED_CAST")
+    private suspend fun loadJar(jarFile: File) {
         val jar = JarFile(jarFile)
 
         val e = jar.entries()
@@ -40,14 +44,18 @@ actual object ModManager {
         while (e.hasMoreElements()) {
             val je = e.nextElement() as JarEntry
 
-            if (je.isDirectory || !je.name.endsWith(".class")) {
-                continue
-            }
-
-            // -6 because of .class
-            val className = je.name.substring(0, je.name.length - 6).replace('/', '.')
-
+            if (je.isDirectory || !je.name.endsWith(".class")) continue
+            val className = je.name.substring(0, je.name.length - ".class".length).replace('/', '.')
             val c: Class<*> = cl.loadClass(className)
+
+            when {
+                c.interfaces.contains(ItemResource::class.java) -> process(c as Class<ItemResource>)
+            }
         }
     }
+}
+
+private suspend fun process(c: Class<ItemResource>) {
+    val items = c.getDeclaredConstructor().newInstance().values()
+    println("Found ${items.size} items")
 }
