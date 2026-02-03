82b912fc42da146b7ad47302677e0b69973b993e
ManApart <r@austinkucera.com>
2026-02-03 14:24:22 -0500

    fix session history formatting

--- src/commonMain/kotlin/core/history/SessionHistory.kt
+++ src/commonMain/kotlin/core/history/SessionHistory.kt
@@ -11,27 +11,29 @@ expect object SessionHistory {
 fun getSessionStats(unknownCommands: Map<String, Int>, eventCounts: List<EventCount>): String {
     var out = "\n## Unknown Commands" +
             "\n\nCommand | Count" +
-            "\n---|---"
+            "\n---|---\n"
     unknownCommands.entries.sortedBy { it.key }.forEach { (command, count) ->
-        out += "$command | $count"
+        out += "$command | $count\n"
     }
+    out +="\n"
 
     out += "\n## Input History" +
             "\n\nTime in seconds | Command" +
-            "\n---|---"
+            "\n---|---\n"
 
     //We could print _all_ histories, but for now just print the main one
     GameLogger.getMainHistory().history.forEach { inOut ->
-        out += "${inOut.timeTaken / 1000f} | ${inOut.input}"
+        out += "${inOut.timeTaken / 1000f} | ${inOut.input}\n"
     }
+    out +="\n"
 
     out +=
         "## Event Counts" +
                 "\n\nEvent | Count" +
-                "\n---|---"
+                "\n---|---\n"
 
     eventCounts.forEach { event ->
-        out += "${event.name} | ${event.count}"
+        out += "${event.name} | ${event.count}\n"
     }
     return out
 }
